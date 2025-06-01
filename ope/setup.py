2c352efe33e7881e41d53214561971b20edb136a
husein-loubani <hussein_lobs@outlook.com>
2025-06-01 22:13:02 +0200

    Update files

---
+++ ope/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ope',
+      version='0.0.2',
+      author='Carlos Manuel Mateo Agullo',
+      author_email='carlos-manuel.mateo-agullo@u-bourgogne.fr',
+      package_dir={'': 'src'},
+      packages=['ope']
+     )
